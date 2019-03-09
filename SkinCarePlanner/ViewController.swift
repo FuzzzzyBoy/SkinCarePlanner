28de8d76d2880d3842910779a040e9fd63312829
Кирилл Шаханский <kshahanskiy@stream.ru>
2019-03-09 12:55:19 +0300

    Initial Commit

---
+++ SkinCarePlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkinCarePlanner
+//
+//  Created by Кирилл Шаханский on 09/03/2019.
+//  Copyright © 2019 Kirill Shalhansky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
