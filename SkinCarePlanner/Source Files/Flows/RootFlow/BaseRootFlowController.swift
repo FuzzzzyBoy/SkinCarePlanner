037f999dafa6edef239e86f7380d2fbed0f02334
Кирилл Шаханский <kshahanskiy@stream.ru>
2019-03-10 17:55:51 +0300

    configure root tab bar controller

--- SkinCarePlanner/Source Files/Flows/RootFlow/BaseRootFlowController.swift
+++ SkinCarePlanner/Source Files/Flows/RootFlow/BaseRootFlowController.swift
@@ -10,13 +10,35 @@ import UIKit
 
 final class BaseRootFlowController: RootFlowController {
 
-    private var rootViewController: UIViewController?
+    private var rootViewController: UITabBarController?
+
+    // MARK: - RootFlowController
 
     func getRootViewController() -> UIViewController {
         if let rootViewController = rootViewController { return rootViewController }
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [
+            getCalendarViewController(),
+            getCatalogViewController()
+        ]
+        return tabBarController
+    }
+
+    // MARK: - Private methods
+
+    private func getCalendarViewController() -> UIViewController {
         let viewController = UIViewController()
         viewController.view.backgroundColor = .red
-        rootViewController = viewController
+        viewController.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 0)
+        viewController.tabBarItem.title = "Календарь"
+        return viewController
+    }
+
+    private func getCatalogViewController() -> UIViewController {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .yellow
+        viewController.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+        viewController.tabBarItem.title = "Каталог"
         return viewController
     }
 
